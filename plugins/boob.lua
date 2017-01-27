98186b913ecb974ff7d6a777fec77c6028048fb6
fiallo1313veeee <aliarjmandi339@yahoo.com>
2017-01-27 13:11:26 +0330

    Update and rename get.lua to boob.lua

---
+++ plugins/boob.lua
@@ -0,0 +1,49 @@
+do
+
+-- Recursive function
+local function getRandomButts(attempt)
+  attempt = attempt or 0
+  attempt = attempt + 1
+
+  local res,status = http.request("http://api.obutts.ru/noise/1")
+
+  if status ~= 200 then return nil end
+  local data = json:decode(res)[1]
+
+  -- The OpenBoobs API sometimes returns an empty array
+  if not data and attempt <= 3 then
+    print('Cannot get that butts, trying another one...')
+    return getRandomButts(attempt)
+  end
+
+  return 'http://media.obutts.ru/' .. data.preview
+end
+
+local function getRandomBoobs(attempt)
+  attempt = attempt or 0
+  attempt = attempt + 1
+
+  local res,status = http.request("http://api.oboobs.ru/noise/1")
+
+  if status ~= 200 then return nil end
+  local data = json:decode(res)[1]
+
+  -- The OpenBoobs API sometimes returns an empty array
+  if not data and attempt < 10 then 
+    print('Cannot get that boobs, trying another one...')
+    return getRandomBoobs(attempt)
+  end
+
+  return 'http://media.oboobs.ru/' .. data.preview
+end
+
+local function run(msg, matches)
+  local url = nil
+  
+  if matches[1] == "ممه" then
+    url = getRandomBoobs()
+  end
+
+  if matches[1] == "کون" then
+    url = getRandomButts()
+  end
